95861ecf17ad77885ec7baf9a3e29eb64c5f5374
Sai <saimhatre243@gmail.com>
2025-08-01 22:45:05 +0530

    Fixed the cors issue

--- server/server.js
+++ server/server.js
@@ -38,7 +38,13 @@ io.on("connection", (socket) => {
 
 // Middleware setup
 app.use(express.json({limit: '4mb'}));
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://chat-app-phi-two-68.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true
+}));
 
 app.use("/api/status", (req, res)=> res.send("Server is live"));
 app.use("/api/auth", userRouter);
